36637d5f027a7d982b1f8087230b255e0c25edb8
nguyenlejoe <joe_le.1@hotmail.com>
2023-12-05 13:56:51 -0800

    create new route for transfers with slug

--- app/dashboard/balance/[slug]/page.tsx
+++ app/dashboard/balance/[slug]/page.tsx
@@ -2,16 +2,15 @@
 import { WalletContext } from "@/lib/store";
 import {
   TokenBalancesListView,
-  TokenTransfersListView
 } from "@covalenthq/goldrush-kit";
 import { SetStateAction, useContext, useEffect, useState } from "react";
-import { Flex } from "@radix-ui/themes";
-import { Button } from "@/components/ui/button";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Balance({ params }: { params: { slug: string } }) {
     const {setWalletAddress, chains} = useContext(WalletContext);
     const [chain_names, setChainNames] = useState(["eth-mainnet"]);
-    const [contractAddress, setContractAddress] = useState("")
+    const pathname = usePathname();
+    const router = useRouter();
 
     useEffect(()=>{
       if(chains.length > 0){
@@ -25,26 +24,12 @@ export default function Balance({ params }: { params: { slug: string } }) {
       }
     },[params.slug])
 
-    return !contractAddress
-        ?
-        <TokenBalancesListView
-          chain_names={chain_names}
-          address={params.slug}
-          onTransferClick={(e: SetStateAction<string>)=>{
-            setContractAddress(e)
-          }}
-        />
-        :
-        <Flex direction="column" gap="2">
-          <TokenTransfersListView
-            chain_name="eth-mainnet"
-            address={params.slug}
-            contract_address={contractAddress}
-          />
-          <div>
-            <Button onClick={()=>{
-              setContractAddress("")
-            }}>Back to balance view</Button>
-          </div>
-        </Flex>
+    return <TokenBalancesListView
+      chain_names={chain_names}
+      address={params.slug}
+      onTransferClick={(e: SetStateAction<string>)=>{
+        router.push(`${pathname}/transfers/${e}`)
+      }}
+    />
+
 }
