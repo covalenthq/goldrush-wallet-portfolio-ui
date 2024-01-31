2622c3faad6d3cec24d8e473f44adc72a30afbb5
Joe Le <55604805+nguyenlejoe@users.noreply.github.com>
2024-01-30 17:05:17 -0800

    Update v0.3.6 (#19)
    * update-v0.3.6
    
    * update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,20 +23,9 @@ module.exports = {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
         },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
         destructive: {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
         },
         popover: {
           DEFAULT: "hsl(var(--popover))",
